4cdaa3d652e6784c9d460b1de24867be4d5a22be
krakos01 <dkrajewski001@outlook.com>
2022-08-14 17:04:53 +0200

    Trying to make a Position list to be radiobuttons. Created class [watch] and tried many ways, but still cannot do them as radiobuttons. Tomorrow will try to finish this problem.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Data.SQLite;
 
 namespace Watch_Precision
 {
@@ -25,27 +26,36 @@ namespace Watch_Precision
         {
             InitializeComponent();
 
+
+            // ResourcesInitialize();
+
             DispatcherTimer timer = new();
             timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += timer_Tick;
+            timer.Tick += Timer_Tick;
             timer.Start();
 
             watchTime.Text = DateTime.Now.ToShortTimeString();
         }
 
-        void timer_Tick(object sender, EventArgs e)
+        void Timer_Tick(object? sender, EventArgs e)
         {
             pcTime.Text = DateTime.Now.ToLongTimeString();
         }
 
-        private void timeUpButton_Click(object sender, RoutedEventArgs e)
+        private void TimeUpButton_Click(object sender, RoutedEventArgs e)
         {
             watchTime.Text = DateTime.Parse(watchTime.Text).AddMinutes(1).ToShortTimeString();
         }
 
-        private void timeDownButton_Click(object sender, RoutedEventArgs e)
+        private void TimeDownButton_Click(object sender, RoutedEventArgs e)
         {
             watchTime.Text = DateTime.Parse(watchTime.Text).AddMinutes(-1).ToShortTimeString();
+        }
+
+       private void ResourcesInitialize()
+        {
+            lbPositions.ItemsSource = Watch;
+
         }
     }
 }
---
+++ Watch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Watch_Precision
+{
+
+    internal class Watch
+    {
+
+        List<string> Positions = new()
+        {
+            "Worn",
+            "On the winder",
+            "Dial up (horizontal)",
+            "Dial down (horizontal)",
+            "12 up (vertical)",
+            "3 up (vertical)",
+            "6 up (vertical)",
+            "9 up (vertical)",
+            "Unspecified"
+        };
+
+        string Name { get; set; }
+        enum Position
+        {
+            Worn,
+            [Display(Name = "On the winder")]
+            Winder,
+            [Display(Name = "Dial up (horizontal)")]
+            DialUp,
+            [Display(Name = "Dial down (horizontal)")]
+            DialDown,
+            [Display(Name = "12 up (vertical)")]
+            Up12,
+            [Display(Name = "3 up (vertical)")]
+            Up3,
+            [Display(Name = "6 up (vertical)")]
+            Up6,
+            [Display(Name = "9 up (vertical)")]
+            Up9,
+            Unspecified
+
+
+        }
+
+    }
+}
